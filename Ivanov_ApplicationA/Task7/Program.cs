4bc5189cad372bfa186c21b960fd5a13e3d088cf
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-04-15 14:24:41 +0300

    Add files via upload

---
+++ Ivanov_ApplicationA/Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+        Console.Write("Введите длину стороны a ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите длину стороны b ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите Высоту трапеции h ");
+        double h = Convert.ToDouble(Console.ReadLine());
+
+        double s = (a+b)/2 * h;
+        Console.WriteLine($"Площадь трапеции = {s}"); ;
+   }
+}
---
+++ Ivanov_ApplicationA/Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите радиус кргуа r=");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        double s = Math.PI * Math.Pow(r, 2);
+        Console.Write($"Площадь круга:{s}");
+    }
+}
