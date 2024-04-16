26bf92f14f51228e21ef5b638d70b61605bc509b
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-04-16 16:45:34 +0300

    Add files via upload

---
+++ Ivanov_Practice1/Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите длину прямоугольника: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите ширину прямоугольника: ");
+        double width = double.Parse(Console.ReadLine());
+
+        double perimeter = 2 * (length + width);
+        double diagonal = Math.Sqrt(length * length + width * width);
+
+        Console.WriteLine("Периметр прямоугольника: " + perimeter);
+        Console.WriteLine("Длина диагонали прямоугольника: " + diagonal);
+    }
+}
---
+++ Ivanov_Practice1/Task12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите целое число N (N > 0): ");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = N; i <= 2 * N; i++)
+        {
+            sum += i * i;
+        }
+
+        Console.WriteLine("Сумма выражения: " + sum);
+    }
+}
