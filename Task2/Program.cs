26e2a75c9dcbb5d393758ca57a4a684887792761
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-04-18 09:51:56 +0300

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A1 = 5, B1 = 3, C1 = 7;
+        double A2 = 2, B2 = 8, C2 = 1;
+
+        Console.WriteLine($"Before sorting: A1={A1}, B1={B1}, C1={C1}");
+        SortDec3(ref A1, ref B1, ref C1);
+        Console.WriteLine($"After sorting: A1={A1}, B1={B1}, C1={C1}");
+
+        Console.WriteLine($"Before sorting: A2={A2}, B2={B2}, C2={C2}");
+        SortDec3(ref A2, ref B2, ref C2);
+        Console.WriteLine($"After sorting: A2={A2}, B2={B2}, C2={C2}");
+        Console.Read();
+    }
+
+    static void SortDec3(ref double A, ref double B, ref double C)
+    {
+        if (A < B)
+        {
+            Swap(ref A, ref B);
+        }
+
+        if (A < C)
+        {
+            Swap(ref A, ref C);
+        }
+
+        if (B < C)
+        {
+            Swap(ref B, ref C);
+        }
+    }
+
+    static void Swap(ref double x, ref double y)
+    {
+        double temp = x;
+        x = y;
+        y = temp;
+    }
+}
---
+++ Task3/Train.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+public class Train
+{
+    public string Number { get; set; }
+    public string Destination { get; set; }
+    public DateTime DepartureTime { get; set; }
+
+    public static bool operator >(Train t1, Train t2)
+    {
+        return t1.DepartureTime > t2.DepartureTime;
+    }
+
+    public static bool operator <(Train t1, Train t2)
+    {
+        return t1.DepartureTime < t2.DepartureTime;
+    }
+}
+
+public class TrainStation
+{
+    private Train[] trains;
+
+    public TrainStation(Train[] trains)
+    {
+        this.trains = trains.OrderBy(t => t.DepartureTime).ToArray();
+    }
+
+    public Train this[string number]
+    {
+        get { return trains.FirstOrDefault(t => t.Number == number); }
+    }
+
+    public Train[] GetTrainsDepartingAfter(DateTime time)
+    {
+        return trains.Where(t => t.DepartureTime > time).ToArray();
+    }
+
+    public Train[] GetTrainsToDestination(string destination)
+    {
+        return trains.Where(t => t.Destination == destination).ToArray();
+    }
+}
