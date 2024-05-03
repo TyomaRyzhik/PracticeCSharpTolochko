3d7447d581916129a09ae5d215254338c8582ea5
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-03 13:06:03 +0300

    Add files via upload

---
+++ Ivanov_Practice13/Task1/Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program2
+{
+    public static void Main2()
+    {
+        Square square = new Square(5);
+        Process(square.Perimeter, square.Side);
+        Process(square.Area, square.Side);
+    }
+
+    public static void Process(Calculate calculate, double side)
+    {
+        Console.WriteLine($"Результат: {calculate(side)}");
+    }
+}
