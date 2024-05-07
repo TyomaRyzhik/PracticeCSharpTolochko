ff60f176a06dfd251fe52a5b4f77e83f3c08722e
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-07 13:48:31 +0300

    Add files via upload

---
+++ Ivanov_Practice17/Practice17/Shape.cs
@@ -0,0 +1,75 @@
+﻿public abstract class Shape
+{
+    public abstract double Area();
+    public abstract double Perimeter();
+}
+
+public class Triangle : Shape
+{
+    public double SideA { get; set; }
+    public double SideB { get; set; }
+    public double SideC { get; set; }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        else
+        {
+            throw new ArgumentException("A triangle with these sides does not exist.");
+        }
+    }
+
+    public override double Area()
+    {
+        double p = (SideA + SideB + SideC) / 2;
+        return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+    }
+
+    public override double Perimeter()
+    {
+        return SideA + SideB + SideC;
+    }
+
+    public string Type()
+    {
+        if (SideA == SideB && SideB == SideC)
+        {
+            return "Equilateral";
+        }
+        else if (SideA == SideB || SideA == SideC || SideB == SideC)
+        {
+            return "Isosceles";
+        }
+        else
+        {
+            return "Scalene";
+        }
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public override double Area()
+    {
+        return Length * Width;
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * (Length + Width);
+    }
+}
---
+++ Ivanov_Practice17/Practice17/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Shape;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем новый объект класса Triangle
+        Triangle myTriangle = new Triangle(3.0, 4.0, 5.0);
+
+        // Выводим информацию о треугольнике
+        Console.WriteLine($"Triangle Type: {myTriangle.Type()}");
+        Console.WriteLine($"Triangle Area: {myTriangle.Area()}");
+        Console.WriteLine($"Triangle Perimeter: {myTriangle.Perimeter()}");
+
+        // Создаем новый объект класса Rectangle
+        Rectangle myRectangle = new Rectangle(5.0, 6.0);
+
+        // Выводим информацию о прямоугольнике
+        Console.WriteLine($"Rectangle Area: {myRectangle.Area()}");
+        Console.WriteLine($"Rectangle Perimeter: {myRectangle.Perimeter()}");
+    }
+}
