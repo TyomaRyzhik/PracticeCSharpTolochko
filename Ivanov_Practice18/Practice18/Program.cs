34c53518d76347b2c5487fed54fa1f258ab0f3ef
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-08 09:43:03 +0300

    Add files via upload

---
+++ Ivanov_Practice18/Practice18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Lorem ###ipsum ######dolor#### Pit ###am##et ##consectetu##########r #adipis######cing### #elit.#### Sed ####malesuada ###########faucibus #########ex ###vec ###. ##Donec ######m#attis #####egestas ########nisi #####non ####pretium. #########Suspendisse ############nec ####eros #####ut ###erat #####faci####lisis#### maximus. #########In ###congue #######et ###Leo# #in ###varius. ########Vestibulum ###########sit ####amet #####felis ######ornare, ########commodo###### #orci #####ut, ####feugiat ########lorem.#####erc!\r\n";
+        Console.WriteLine(ProcessString(input));
+    }
+
+    static string ProcessString(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c == '#')
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        char[] result = stack.ToArray();
+        Array.Reverse(result);
+        return new string(result);
+    }
+}
---
+++ Ivanov_Practice18/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10, b = 20;
+        string path = "C:\\Users\\USER\\Desktop\\36ТП\\3 курс\\Практика Толочко\\Ivanov_Practice18\\Task2\\numbers.txt";
+
+        Queue<int> lessThanA = new Queue<int>();
+        Queue<int> betweenAB = new Queue<int>();
+        Queue<int> greaterThanB = new Queue<int>();
+
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                int number;
+                if (int.TryParse(line, out number))
+                {
+                    if (number < a)
+                    {
+                        lessThanA.Enqueue(number);
+                    }
+                    else if (number > b)
+                    {
+                        greaterThanB.Enqueue(number);
+                    }
+                    else
+                    {
+                        betweenAB.Enqueue(number);
+                    }
+                }
+            }
+        }
+
+
+        PrintQueue(betweenAB);
+        PrintQueue(lessThanA);
+        PrintQueue(greaterThanB);
+    }
+
+    static void PrintQueue(Queue<int> queue)
+    {
+        while (queue.Count > 0)
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
