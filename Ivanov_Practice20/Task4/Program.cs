b901e6163e6bc7017045bbbe15fe6cae71077159
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-10 10:07:14 +0300

    Add files via upload

---
+++ Ivanov_Practice20/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double start = -1;
+        double end = 9;
+        double step = 1;
+
+        int steps = (int)((end - start) / step);
+
+        Parallel.For(0, steps, i =>
+        {
+            double x = start + i * step;
+            if (x != 0)
+            {
+                double result = Math.Cos(1 / x);
+                Console.WriteLine($"cos(1/{x}) = {result}");
+            }
+        });
+    }
+}
---
+++ Ivanov_Practice20/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 34, 7, 87, 12 };
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        try
+        {
+            Parallel.ForEach(numbers, new ParallelOptions { CancellationToken = cts.Token }, number =>
+            {
+                int sum = Enumerable.Range(0, number + 1).Sum();
+                int product = Enumerable.Range(1, number + 1).Aggregate(1, (a, b) => a * b);
+
+                Console.WriteLine($"Number: {number}, Sum: {sum}, Product: {product}");
+
+                if (sum > 535 || product > 535)
+                {
+                    cts.Cancel();
+                }
+            });
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Вычисление прервано");
+        }
+    }
+}
