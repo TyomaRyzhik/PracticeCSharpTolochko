44757b8c49f87af7c4b4ccc180856a925724ae64
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-11 08:49:12 +0300

    Add files via upload

---
+++ Ivanov_Practice21/Task1/ChocolateBoiler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class ChocolateBoiler
+{
+    private bool empty;
+    private bool boiled;
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler()
+    {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler GetInstance()
+    {
+        if (uniqueInstance == null)
+        {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+    public void Fill()
+    {
+        if (IsEmpty())
+        {
+            empty = false;
+            boiled = false;
+            // Заполнение бойлера молочно-шоколадной смесью
+        }
+    }
+
+    public void Drain()
+    {
+        if (!IsEmpty() && IsBoiled())
+        {
+            // Слив готового молока и шоколада
+            empty = true;
+        }
+    }
+
+
+    public void Boil()
+    {
+        if (!IsEmpty() && !IsBoiled())
+        {
+            // Вскипятить содержимое бойлера
+            boiled = true;
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return empty;
+    }
+
+    public bool IsBoiled()
+    {
+        return boiled;
+    }
+}
---
+++ Ivanov_Practice21/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ChocolateBoiler boiler = ChocolateBoiler.GetInstance();
+
+        boiler.Fill(); 
+        boiler.Boil(); 
+
+        bool isEmpty = boiler.IsEmpty();
+        Console.WriteLine("Бойлер пуст: " + isEmpty);
+
+        boiler.Drain();
+
+        isEmpty = boiler.IsEmpty();
+        Console.WriteLine("Бойлер пуст: " + isEmpty);
+
+        Console.ReadLine();
+    }
+}
