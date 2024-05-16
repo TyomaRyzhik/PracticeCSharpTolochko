47cdf87010dd95c612321c6df674924c77831172
Артём <98823560+TyomaRyzhik@users.noreply.github.com>
2024-05-16 14:53:49 +0300

    Add files via upload

---
+++ Ivanov_Practice23/Task1/Form1.cs
@@ -0,0 +1,22 @@
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Changer_Click(object sender, EventArgs e)
+        {
+            if (listWords.SelectedItem != null)
+            {
+                string input = listWords.SelectedItem.ToString();
+                string output = Regex.Replace(input, "[a-zA-Z]", "+");
+                Result.Text = output;
+            }
+        }
+    }
+}
